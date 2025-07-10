67f01f19fde9eaf7bb15b3fe01edcb7aca7f30d9
Maycow Meira <maycowmeira@gmail.com>
2025-07-10 12:18:09 +0200

    Fix dark mode toggle and typos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-  const toogle_button = document.querySelector('div.header-right .icons li:last-child a.button');
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleButton = document.querySelector('div.header-right .icons li:last-child a.button');
 
-  toogle_button.removeAttribute('href');
-
-  toogle_button.onclick = function () {
-    if (document.body.classList.value == 'dark') {
-      document.body.classList.remove('dark');
-    } else {
-      document.body.classList.add('dark');
-    }
+  if (!toggleButton) {
+    return;
   }
+
+  toggleButton.removeAttribute('href');
+
+  toggleButton.onclick = function () {
+    document.body.classList.toggle('dark');
+  };
 
   const preferDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
